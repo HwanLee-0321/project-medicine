6d5125ef6e40cc7db2c7c643e7a2cdec35ed0db6
HwanLee-0321 <ghksdl413@naver.com>
2025-08-08 10:18:07 +0900

    백엔드 삭제

--- backend/routes/memberRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { signup, login, remove } = require('../controllers/memberCotroller');
-
-router.post('/signup', signup);
-router.post('/login', login);
-router.post('/delete', remove);
-
-module.exports = router;
