9e1517b43004daef2aeb04ef9f050a8092874ff0
zye1oo <bjh4213@naver.com>
2025-08-26 17:33:59 +0900

    get->post

--- backend/routes/medicationRouter.js
+++ backend/routes/medicationRouter.js
@@ -7,6 +7,6 @@ router.post('/ocr', handleOCRData);  // 복약 정보
 router.post('/ocr/read', readOCRData);  // 복약 정보 조회
 router.post('/schedule', handleSchedule);  // 복약 스케줄 관리
 router.post('/time', mealTime);  // 복약 시간, 정보 저장 
-router.get('/time/read', readMealTime);  // 복약 일정 조회
+router.post('/time/read', readMealTime);  // 복약 일정 조회
 
 module.exports = router; 
